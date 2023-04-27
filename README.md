678dd8ca58c68566833dbdae7da0ea6951219f1e
Josh Mandel <jmandel@alum.mit.edu>
2023-04-27 10:00:02 -0500

    Take a series of input files and/or stdin

--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 
 Web demo at https://joshuamnadel.com/fhir-view-to-array
 
-Or run CLI with Deno
+Or run CLI with Deno...
+
+###  Generate a CSV file
 
 ```sh
 cat pt.ndjson | \
 deno run --allow-read generate-table.ts \
   --config patient-contacts-config.json \
+  --stdin \
 > contacts.csv
 ```
+
+Or pass a series of `--infile filename.ndjson` arguments to read from the filesystem.
+
+###  Generate a sqlite table
+
+Add or replace a table in `analysis.db` (creating the file if necessary)
+
+```sh
+deno run --allow-read --allow-write generate-table.ts \
+  --config patient-contacts-config.json \
+  --infile pt.ndjson \
+  --sqlite analysis.db
+```
+
+Use `--append` flag to append to an existing table.
--- generate-table.ts
+++ generate-table.ts
@@ -1,12 +1,12 @@
-// generate-table.ts
-
-import { parse } from "https://deno.land/std@0.184.0/flags/mod.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts";
 import { processResources, fromNdjsonResponse } from "./processor.js";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { readableStreamFromReader } from "https://deno.land/std@0.171.0/streams/mod.ts";
 
-const args = parse(Deno.args);
+const args = parse(Deno.args, {collect: "infile"});
 
 if (!args.config) {
-  console.error("Usage: cat pt.ndjson | deno run --allow-read generate-table.ts --config patient-contacts-config.json");
+  console.error("Usage: deno run --allow-read generate-table.ts --config myconfig.json [--sqlite mydb.sqlite] [--table mytable] [--append]");
   Deno.exit(1);
 }
 
@@ -14,18 +14,69 @@ const configPath = args.config;
 const configFile = await Deno.readTextFile(configPath);
 const config = JSON.parse(configFile);
 
+const useSqlite = !!args.sqlite;
+const sqliteFile = args.sqlite;
+const append = !!args.append;
+const stdin = !!args.stdin;
+const infile = args.infile;
+console.log("inf", infile)
+
+const tableName = args.table || config.view || config.name || "output_table";
+
+if (useSqlite && !tableName) {
+  console.error("Please provide a table name with --table option");
+  Deno.exit(1);
+}
+
 async function main() {
-  const resourceGenerator = fromNdjsonResponse(new Response(Deno.stdin.readable));
 
-  const processor = processResources(resourceGenerator, config);
+  const sources = [
+    ...infile.map(f => readableStreamFromReader(Deno.openSync(f, {read: true}))),
+    ...(stdin ? [Deno.stdin.readable] : [])
+  ]
 
-  // Print header (column names)
-  console.log(Object.keys(config.columns).map(quote).join(","));
+  async function* resources(streams) {
+    for (const s of streams ) {
+      yield* fromNdjsonResponse(new Response(s));
+    }
+  }
 
-  // Process and print rows
+  const processor = processResources(resources(sources), config);
+
+  if (useSqlite) {
+    const db = new DB(sqliteFile);
+    db.execute("BEGIN TRANSACTION")
+    createTable(db, tableName, config.columns, append);
+    await insertData(db, tableName, config.columns, processor);
+    db.execute("END")
+    db.close()
+  } else {
+    printCsv(config.columns, processor);
+  }
+}
+
+async function printCsv(columns, processor) {
+  console.log(Object.keys(columns).map(quote).join(","));
   for await (const row of processor) {
     console.log(row.map(quote).join(","));
   }
+}
+
+function createTable(db, tableName, columns, append) {
+  const columnDefs = Object.keys(columns).map((col) => `${col} TEXT`).join(", ");
+  if (!append) {
+    db.execute(`DROP TABLE IF EXISTS ${tableName}`);
+  }
+  db.execute(`CREATE TABLE IF NOT EXISTS ${tableName} (${columnDefs})`);
+}
+
+async function insertData(db, tableName, columns, processor) {
+  const placeholders = Object.keys(columns).map(() => "?").join(", ");
+    const q = db.prepareQuery(`INSERT INTO ${tableName} VALUES (${placeholders})`)
+    for await (const row of processor) {
+      q.execute(row);
+    }
+    q.finalize()
 }
 
 function quote(value) {
